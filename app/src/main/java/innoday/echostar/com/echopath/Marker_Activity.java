c3df28af8b88b99bf2d8ff00f5ce5593aa8694dc
sheshank.kodam <sheshank.kodam@echostar.com>
2016-03-15 15:56:59 -0600

    1. Added polyline between markers

--- app/src/main/java/innoday/echostar/com/echopath/Marker_Activity.java
+++ app/src/main/java/innoday/echostar/com/echopath/Marker_Activity.java
@@ -4,6 +4,7 @@ package innoday.echostar.com.echopath;
  * Created by Sheshank.Kodam on 2/19/2016.
  */
 import android.app.Fragment;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,10 +15,13 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.util.ArrayList;
 
 public class Marker_Activity extends Fragment {
+    ArrayList<LatLng> locations = new ArrayList();
     GoogleMap googleMap;
-    private static final LatLng Echostar = new LatLng(39.575353,-104.864334);
 
     public void processMap(View v){
         if(googleMap == null){
@@ -25,8 +29,21 @@ public class Marker_Activity extends Fragment {
         }
 
         if(googleMap != null){
-            googleMap.addMarker(new MarkerOptions().position(Echostar).title("EchoStar"));
-            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Echostar,100));
+            locations.add(new LatLng(39.575353, -104.864334));
+            locations.add(new LatLng(39.575350, -104.864309));
+            locations.add(new LatLng(39.575322, -104.864273));
+            for(LatLng location: locations){
+                googleMap.addMarker(new MarkerOptions().position(location).title("EchoStar"));
+                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 100));
+            }
+            // Instantiating the class PolylineOptions to plot polyline in the map
+            PolylineOptions polylineOptions = new PolylineOptions();
+
+            // drawing polyline
+            polylineOptions.color(Color.BLUE);
+            polylineOptions.width(7);
+            polylineOptions.addAll(locations);
+            googleMap.addPolyline(polylineOptions);
         }
     }
 
@@ -37,3 +54,5 @@ public class Marker_Activity extends Fragment {
         return v;
     }
 }
+
+
