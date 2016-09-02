a14ef369aebaf3b4657a0fc4494d94ee9b6b4073
sheshank.kodam <sheshank.kodam@echostar.com>
2016-09-01 20:00:38 -0600

    Changed url Refactoring

--- app/src/main/java/innoday/echostar/com/echopath/MainActivity.java
+++ app/src/main/java/innoday/echostar/com/echopath/MainActivity.java
@@ -31,26 +31,26 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Spinner fromSpinner = (Spinner) findViewById(R.id.from);
-                Location fromLocation = new Location();
-                Location toLocation = new Location();
-                try{
-                    fromLocation = (Location) fromMeetingRoomObj.get(fromSpinner.getSelectedItem().toString());
-                } catch (JSONException e){
-                    Log.e("onClick: ", e.getMessage(), e);
-                }
                 Spinner toSpinner = (Spinner) findViewById(R.id.to);
-                try{
-                    toLocation = (Location) toMeetingRoomObj.get(toSpinner.getSelectedItem().toString());
-                } catch (JSONException e){
-                    Log.e("onClick: ", e.getMessage(), e);
-                }
-                new ShortestDistanceTask(fromLocation, toLocation).execute();
 
+                Location fromLocation = getMeetingRoomValues(fromSpinner, fromMeetingRoomObj);
+                Location toLocation = getMeetingRoomValues(toSpinner, toMeetingRoomObj);
+                new ShortestDistanceTask(fromLocation, toLocation).execute();
             }
         });
 
     }
 
+    public Location getMeetingRoomValues(Spinner spinner, JSONObject meetingRoomObj){
+        Location meetingRoomValues = new Location();
+        try{
+            meetingRoomValues = (Location) meetingRoomObj.get(spinner.getSelectedItem().toString());
+        } catch (JSONException e){
+            Log.e("onClick: ", e.getMessage(), e);
+        }
+
+        return meetingRoomValues;
+    }
 
     @Override
     protected void onStart() {
@@ -58,36 +58,29 @@ public class MainActivity extends AppCompatActivity {
         new HttpRequestTask().execute();
     }
 
-    public void setAdaptor(JSONObject fromMeetingRoomObj, JSONObject toMeetingRoomObj){
-
-        JSONArray fromMeetingRoomsArray = fromMeetingRoomObj.names();
-        List fromMeetingRooms = new ArrayList();
-        for (int i = 0; i < fromMeetingRoomsArray.length(); i++) {
-            try {
-                fromMeetingRooms.add(fromMeetingRoomsArray.get(i));
-            }catch (JSONException e){
-                Log.e("setAdaptor: ",e.getMessage(), e);
-            }
-        }
-
-        JSONArray toMeetingRoomsArray = toMeetingRoomObj.names();
-        List toMeetingRooms = new ArrayList();
-        for(int i = 0; i < toMeetingRoomsArray.length(); i++){
-            try{
-                toMeetingRooms.add(toMeetingRoomsArray.get(i));
-            }catch (JSONException e){
-                Log.e("setAdaptor: ",e.getMessage(), e);
-            }
-
-        }
-
+    public void setSpinners(){
         Spinner from = (Spinner)findViewById(R.id.from);
-        ArrayAdapter<String> fromAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item, fromMeetingRooms);
+        List fromMeetingRoomNames = getMeetingRoomNames(fromMeetingRoomObj);
+        ArrayAdapter<String> fromAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item, fromMeetingRoomNames);
         from.setAdapter(fromAdapter);
 
         Spinner to = (Spinner)findViewById(R.id.to);
-        ArrayAdapter<Location> toAdapter = new ArrayAdapter<Location>(this,android.R.layout.simple_spinner_dropdown_item,toMeetingRooms);
+        List toMeetingRoomNames = getMeetingRoomNames(toMeetingRoomObj);
+        ArrayAdapter<Location> toAdapter = new ArrayAdapter<Location>(this,android.R.layout.simple_spinner_dropdown_item, toMeetingRoomNames);
         to.setAdapter(toAdapter);
+    }
+
+    public List getMeetingRoomNames(JSONObject meetingRoomObj){
+        List meetingRoomNames = new ArrayList();
+        JSONArray meetingRoomsKeys = meetingRoomObj.names();
+        for (int i = 0; i < meetingRoomsKeys.length(); i++) {
+            try {
+                meetingRoomNames.add(meetingRoomsKeys.get(i));
+            }catch (JSONException e){
+                Log.e("setSpinners: ",e.getMessage(), e);
+            }
+        }
+        return meetingRoomNames;
     }
 
     private class HttpRequestTask extends AsyncTask<Void, Void, LocationsDTO> {
@@ -95,7 +88,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected LocationsDTO doInBackground(Void... params) {
             try {
-                final String url = "http://10.73.81.104:8080/echopath/location/locationsOnly";
+                final String url = "http://10.73.72.140:8080/echopath/location/locationsOnly";
 
                 RestTemplate restTemplate = new RestTemplate();
                 restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
@@ -122,23 +115,17 @@ public class MainActivity extends AppCompatActivity {
                 }catch (JSONException e) {
                     Log.e("MainActivity", e.getMessage(), e);}
             }
-            setAdaptor(fromMeetingRoomObj, toMeetingRoomObj);
+            setSpinners();
         }
 
     }
 
     public class ShortestDistanceTask extends AsyncTask<Void, Void, TempShortestPath> {
 
-        private static final String BASE_URL = "http://10.73.81.104:8080/echopath/location/";
-
-
+        private static final String BASE_URL = "http://10.73.72.140:8080/echopath/location/";
         private Location fromLocation;
-
         private  Location toLocation;
-
-
         private TempShortestPath shortestPathDTO = new TempShortestPath();
-
 
         public ShortestDistanceTask(Location fromLocation, Location toLocation){
               this.fromLocation = fromLocation;
